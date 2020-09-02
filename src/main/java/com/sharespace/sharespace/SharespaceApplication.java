4f9e450a82ead833c69cfd5ba26807520b5315c3
unknown <djsboark@gmail.com>
2020-09-02 22:21:24 +0900

    first commit

---
+++ src/main/java/com/sharespace/sharespace/SharespaceApplication.java
@@ -0,0 +1,13 @@
+package com.sharespace.sharespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharespaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharespace/sharespace/SharespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharespace.sharespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
